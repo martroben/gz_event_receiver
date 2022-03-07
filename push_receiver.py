31313467d634900dd978dde3b266ed2a343fc861
martroben <87522742+martroben@users.noreply.github.com>
2022-03-07 22:17:44 +0200

    Update push_receiver.py
    Set /status to also reveal headers to check if chunked encoding in working.

--- push_receiver.py
+++ push_receiver.py
@@ -8,18 +8,26 @@ t1 = perf_counter()
 app = Flask(__name__)
 
 last_request = None
+headers = None
 
 @app.route("/status", methods=["GET"])
 def status():
   """URL to get status info via browser"""
+  
   uptime_minutes = (perf_counter() - t1) / 60
-  return """"service online<br>
-             uptime: {:0.2f} min<br>
-             last request: {}""".format(uptime_minutes, last_request)
+  return """service online<br>
+            uptime: {:0.2f} min<br>
+            last request: {}<br>
+            headers: {}""".format(uptime_minutes, last_request, headers)
+
 
 @app.route("/receiver", methods=["POST"])
 def receiver():
-  """URL to receive POST requests. Parse content from json to dict"""
+  """POST request receiver URL. Parse json content as dict"""
+  
   global last_request
   last_request = request.json
+
+  global headers
+  headers = request.headers
   return "OK"
