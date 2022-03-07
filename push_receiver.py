fc81d96ba8e7ad6e53a760b4300916d46f8f7113
martroben <87522742+martroben@users.noreply.github.com>
2022-03-07 22:34:33 +0200

    Update push_receiver.py
    Edited variable names, formatted output text.

--- push_receiver.py
+++ push_receiver.py
@@ -7,27 +7,27 @@ t1 = perf_counter()
 # Create Flask object
 app = Flask(__name__)
 
-last_request = None
+body = None
 headers = None
-
-@app.route("/status", methods=["GET"])
-def status():
-  """URL to get status info via browser"""
-  
-  uptime_minutes = (perf_counter() - t1) / 60
-  return """service online<br>
-            uptime: {:0.2f} min<br>
-            last request: {}<br>
-            headers: {}""".format(uptime_minutes, last_request, headers)
-
 
 @app.route("/receiver", methods=["POST"])
 def receiver():
-  """POST request receiver URL. Parse json content as dict"""
+  """accept POST requests by /receiver URL"""
   
-  global last_request
-  last_request = request.json
-
+  global body
+  body = request.json
+  
   global headers
   headers = request.headers
   return "OK"
+
+@app.route("/status", methods=["GET"])
+def status():
+  """get status and last request by /status URL in browser"""
+  
+  uptime_minutes = (perf_counter() - t1) / 60
+  return """service online<br>
+            uptime: {:0.2f} min<br><br>
+            last request:<br>
+            headers: {}<br>
+            body: {}""".format(uptime_minutes, headers, body)
